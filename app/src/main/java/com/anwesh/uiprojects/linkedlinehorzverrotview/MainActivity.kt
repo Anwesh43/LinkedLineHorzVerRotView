9cda59f0b29035dbc12c897895b23c935df15cbf
Anwesh43 <anweshthecool0@gmail.com>
2019-05-18 13:35:23 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinehorzverrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinehorzverrotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinehorzverrotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linehorzverrotview.LineHorzVerRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineHorzVerRotView.create(this)
     }
 }
